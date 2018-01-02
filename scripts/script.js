cf852929f125a111fafb537b8357e79c44c774e8
Tristan Norton <tristan.m.norton@gmail.com>
2018-01-02 12:47:16 -0500

    Applied midpoint function. Now I need if I can apply coordinates to and image

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,25 @@ $( document ).ready(function() {
      
     var docHeight = $(document).height();
     var docWidth = $(document).width(); 
-    
-    console.log(docHeight);
+    var viewPortMidPointX = (docWidth / 2);
+    var viewPortMidPointY = (docHeight / 2);
+
  
     $("#wrapper").on('mousemove', function(e){
         
         var xAxis = e.pageX;
         var yAxis = e.pageY;
-        
+        var currentPosition = findMidpoint(xAxis, yAxis);
+
         $('#playerToon').css({
                left: xAxis,
                top: yAxis
            
+       });
+       
+       $('#playerToon2').css({
+            left: currentPosition[0],
+            top: currentPosition[1]
        });
     });
     $("#wrapper").on('mousemove', function(e){
@@ -23,5 +30,15 @@ $( document ).ready(function() {
         var xAxis = e.pageX;
         var yAxis = e.pageY;
     });
- 
+    
+    /* 
+    Passes current cursor location and calculates midpoint of viewport.
+    Will also create a function to calculate distance and amount of midway movement. 
+    */
+    var findMidpoint = function(xAxisForFunct, yAxisForFunct, distance) {
+        var arrayCoord = [];
+        arrayCoord[0] = ((xAxisForFunct + viewPortMidPointX) / 2);
+        arrayCoord[1] = ((yAxisForFunct + viewPortMidPointY) / 2);
+        return arrayCoord
+    };
 });
