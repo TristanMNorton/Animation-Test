baabbbf2220d1aefea7715323e95e1222ca33721
Tristan Norton <tristan.m.norton@gmail.com>
2018-01-02 13:38:28 -0500

    My theory crumbled. Back to the drawing board.

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,9 @@ $( document ).ready(function() {
     var viewPortMidPointX = (docWidth / 2);
     var viewPortMidPointY = (docHeight / 2);
 
- 
+    $()
+
+
     $("#wrapper").on('mousemove', function(e){
         
         var xAxis = e.pageX;
@@ -17,12 +19,15 @@ $( document ).ready(function() {
         $('#playerToon').css({
                left: xAxis,
                top: yAxis
-           
        });
        
        $('#playerToon2').css({
             left: currentPosition[0],
             top: currentPosition[1]
+       });
+       $('.toonContainer').children().css({
+            left:-(this.clientWidth / 4),
+            top: -(this.clientHeight / 4)
        });
     });
     $("#wrapper").on('mousemove', function(e){
